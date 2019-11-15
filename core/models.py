c5e3262fa4f042857140ba1a25bd6b76ca119157
raydeal <alnw@interia.eu>
2019-11-15 13:13:58 +0100

    improve models

--- core/models.py
+++ core/models.py
@@ -4,9 +4,17 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 
+class QuestionManager(models.Manager):
+
+    def ordinal_ordered(self):
+        return self.get_queryset().order_by('ordinal_number')
+
+
 class User(AbstractUser):
     uuid = models.UUIDField(default=uuid.uuid4, editable=False)
     phone = models.CharField(max_length=20, null=True, blank=True)
+    # 2-letter country name abbreviations
+    country = models.CharField(max_length=2, default='UK')
 
     class Meta:
         db_table = 'auth_user'
@@ -18,6 +26,9 @@ class User(AbstractUser):
 class Question(models.Model):
     title = models.CharField(max_length=250, unique=True)
     user = models.ManyToManyField(User, through='Answer')
+    ordinal_number = models.PositiveSmallIntegerField(null=True, blank=True)
+
+    objects = QuestionManager()
     
     def __str__(self):
         return self.title[:10]
@@ -27,6 +38,7 @@ class Answer(models.Model):
     user = models.ForeignKey(User, on_delete=models.PROTECT)
     question = models.ForeignKey(Question, on_delete=models.PROTECT)
     answer = models.TextField()
+    language = models.CharField(max_length=5, default='en-gb')
 
     class Meta:
         unique_together = [['user', 'question']]
